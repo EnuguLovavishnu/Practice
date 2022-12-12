3647bd1215495688afde88c5e7c77a93afb61612
EnuguLovavishnu <enugulovavishnu@gmail.com>
2022-12-12 12:39:55 +0530

    initial commit

---
+++ a.py
@@ -0,0 +1,5 @@
+a=int(input("enter a string"))
+if a%2==0:
+    print("weid")
+    else:
+        print("not weid")
---
+++ num.py
@@ -0,0 +1,5 @@
+n=int(input("enter a number"))
+for i in range (n):
+    if n>20:
+        i=n**2
+        print("n")
